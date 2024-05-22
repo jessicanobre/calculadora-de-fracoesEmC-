67559fa54698108740e7b556be42daa20f1426dc
jessicanobre <150728052+jessicanobre@users.noreply.github.com>
2024-05-22 01:02:25 +0000

    a

---
+++ calculadora.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+int main (){
+ float numerador, denominador; 
+ int operacao;
+ printf("Digite o numerador da primeira fração: ");
+ scanf("%f", &numerador);
+//
+ printf("Digite o denominador da primeira fração: ");
+ scanf("%f", &denominador);
+//
+ printf("Escolha a operação 1 + , 2 - , 3 * , 4 / ");
+ scanf("%d", &operacao);
+
+return 0;
+}
